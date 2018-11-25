64bcfcac85f4b3b7ebc563b85f865219b17a12c7
fin <nayanzin.alexander@gmail.com>
2018-11-25 12:59:00 +0200

    initial

---
+++ src/main/java/com/nayanzin/springbootjooq/SpringBootJooqApplication.java
@@ -0,0 +1,12 @@
+package com.nayanzin.springbootjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJooqApplication.class, args);
+    }
+}
